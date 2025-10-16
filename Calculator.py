f326fd53e875e513a94dfff2bcf4f25c26ea625a
Vaibhavi Pardeshi <pardeshivaibhavi8@gmail.com>
2025-10-16 20:12:59 +0530

    Update Calculator and add Password Generator

--- Calculator.py
+++ Calculator.py
@@ -10,55 +10,31 @@ class Calculator:
         self.equation = StringVar()
         self.entry_value = ''
 
-        # Entry box
+        
         Entry(master, width=17, bg='#fff', font=('Arial Bold', 28), 
               textvariable=self.equation).place(x=0, y=0)
 
-        # Buttons
-        Button(master, width=11, height=4, text='(', relief='flat', bg='black',
-               fg='white', command=lambda: self.show('(')).place(x=0, y=50)
-        Button(master, width=11, height=4, text=')', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(')')).place(x=90, y=50)
-        Button(master, width=11, height=4, text='%', relief='flat', bg='black',
-               fg='white', command=lambda: self.show('%')).place(x=180, y=50)
-        Button(master, width=11, height=4, text='/', relief='flat', bg='black',
-               fg='white', command=lambda: self.show('/')).place(x=270, y=50)
-
-        Button(master, width=11, height=4, text='1', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(1)).place(x=0, y=125)
-        Button(master, width=11, height=4, text='2', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(2)).place(x=90, y=125)
-        Button(master, width=11, height=4, text='3', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(3)).place(x=180, y=125)
-        Button(master, width=11, height=4, text='*', relief='flat', bg='black',
-               fg='white', command=lambda: self.show('*')).place(x=270, y=125)
-
-        Button(master, width=11, height=4, text='4', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(4)).place(x=0, y=200)
-        Button(master, width=11, height=4, text='5', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(5)).place(x=90, y=200)
-        Button(master, width=11, height=4, text='6', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(6)).place(x=180, y=200)
-        Button(master, width=11, height=4, text='-', relief='flat', bg='black',
-               fg='white', command=lambda: self.show('-')).place(x=270, y=200)
-
-        Button(master, width=11, height=4, text='7', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(7)).place(x=0, y=275)
-        Button(master, width=11, height=4, text='8', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(8)).place(x=90, y=275)
-        Button(master, width=11, height=4, text='9', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(9)).place(x=180, y=275)
-        Button(master, width=11, height=4, text='+', relief='flat', bg='black',
-               fg='white', command=lambda: self.show('+')).place(x=270, y=275)
-
-        Button(master, width=11, height=4, text='0', relief='flat', bg='black',
-               fg='white', command=lambda: self.show(0)).place(x=90, y=350)
-        Button(master, width=11, height=4, text='.', relief='flat', bg='black',
-               fg='white', command=lambda: self.show('.')).place(x=180, y=350)
-        Button(master, width=11, height=4, text='=', relief='flat', bg='lightblue',
-               command=self.solve).place(x=270, y=350)
-        Button(master, width=11, height=4, text='AC', relief='flat', bg='red',
-               fg='white', command=self.clear).place(x=0, y=350)
+        
+        Button(master, width=11, height=4, text='(', relief='flat', bg='black', fg='white', command=lambda: self.show('(')).place(x=0, y=50)
+        Button(master, width=11, height=4, text=')', relief='flat', bg='black', fg='white', command=lambda: self.show(')')).place(x=90, y=50)
+        Button(master, width=11, height=4, text='%', relief='flat', bg='black', fg='white', command=lambda: self.show('%')).place(x=180, y=50)
+        Button(master, width=11, height=4, text='/', relief='flat', bg='black', fg='white', command=lambda: self.show('/')).place(x=270, y=50)
+        Button(master, width=11, height=4, text='1', relief='flat', bg='black', fg='white', command=lambda: self.show(1)).place(x=0, y=125)
+        Button(master, width=11, height=4, text='2', relief='flat', bg='black', fg='white', command=lambda: self.show(2)).place(x=90, y=125)
+        Button(master, width=11, height=4, text='3', relief='flat', bg='black', fg='white', command=lambda: self.show(3)).place(x=180, y=125)
+        Button(master, width=11, height=4, text='4', relief='flat', bg='black', fg='white', command=lambda: self.show(4)).place(x=0, y=200)
+        Button(master, width=11, height=4, text='5', relief='flat', bg='black', fg='white', command=lambda: self.show(5)).place(x=90, y=200)
+        Button(master, width=11, height=4, text='6', relief='flat', bg='black', fg='white', command=lambda: self.show(6)).place(x=180, y=200)
+        Button(master, width=11, height=4, text='7', relief='flat', bg='black', fg='white', command=lambda: self.show(7)).place(x=0, y=275)
+        Button(master, width=11, height=4, text='8', relief='flat', bg='black', fg='white', command=lambda: self.show(8)).place(x=90, y=275)
+        Button(master, width=11, height=4, text='9', relief='flat', bg='black', fg='white', command=lambda: self.show(9)).place(x=180, y=275)
+        Button(master, width=11, height=4, text='0', relief='flat', bg='black', fg='white', command=lambda: self.show(0)).place(x=90, y=350)
+        Button(master, width=11, height=4, text='+', relief='flat', bg='black', fg='white', command=lambda: self.show('+')).place(x=270, y=275)
+        Button(master, width=11, height=4, text='X', relief='flat', bg='black', fg='white', command=lambda: self.show('X')).place(x=270, y=125)
+        Button(master, width=11, height=4, text='-', relief='flat', bg='black', fg='white', command=lambda: self.show('-')).place(x=270, y=200)
+        Button(master, width=11, height=4, text='.', relief='flat', bg='black', fg='white', command=lambda: self.show('.')).place(x=180, y=350)
+        Button(master, width=11, height=4, text='=', relief='flat', bg='lightblue', command=self.solve).place(x=270, y=350)
+        Button(master, width=11, height=4, text='AC', relief='flat', bg='red', fg='white', command=self.clear).place(x=0, y=350)
 
     def show(self, value):
         self.entry_value += str(value)
@@ -78,7 +54,7 @@ class Calculator:
             self.entry_value = ''
 
 
-# Run the calculator
+
 root = Tk()
 calc = Calculator(root)
 root.mainloop()
